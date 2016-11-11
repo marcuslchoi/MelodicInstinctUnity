5ede53c1de42de65b83d9c7236d800b0ee5e23da
marcus <marcuslchoi@gmail.com>
2016-11-11 13:31:22 -0800

    added helicopter

---
+++ Assets/MyScripts/HelicopterHover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterHover : MonoBehaviour {
+
+	float speed;
+
+	void Update () {
+
+		if ((Mathf.Floor (Time.time) % 2 == 1))
+			speed = 20f;
+		else
+			speed = -20f;
+	
+		transform.Translate (Vector3.up * Time.deltaTime * speed);
+	}
+}
---
+++ Assets/MyScripts/HelicopterMainRotorAnimate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterMainRotorAnimate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	//TODO: use OnEnable
+
+	float speed = 10000f;
+
+	void Update ()
+	{
+		transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+	}
+}
