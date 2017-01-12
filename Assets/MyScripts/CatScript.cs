6ce3385a844f18546b8b7557a65d36203efd8439
Marcus Choi <marcuslchoi@gmail.com>
2017-01-11 20:32:26 -0800

    added cat script for “me” syllable

---
+++ Assets/MyScripts/CatScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatScript : MonoBehaviour {
+
+	private float _speed = 100f;
+	private float _yRotation = .30f;
+
+	//rotate y between yRot and -yRot
+	void Update () {
+
+		if (Mathf.Abs(transform.localRotation.y) > _yRotation)
+			_speed = -_speed;
+
+		transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+	}
+}
