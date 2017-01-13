b4ebc533ca4028351b792d7bfaf7ebae34ac3afa
Marcus Choi <marcuslchoi@gmail.com>
2017-01-12 18:27:22 -0800

    added tears to the cat

---
+++ Assets/TearScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TearScript : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void Start()
+	{
+		rb.AddForce (Vector3.down* 5000f);
+	}
+}
