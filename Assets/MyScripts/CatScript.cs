39f634c9690749d9ee8613f8d68d0ad899496d3b
Marcus Choi <marcuslchoi@gmail.com>
2017-01-12 19:30:36 -0800

    tears instantiated from cat’s eyes

--- Assets/MyScripts/CatScript.cs
+++ Assets/MyScripts/CatScript.cs
@@ -6,12 +6,46 @@ public class CatScript : MonoBehaviour {
 	private float _speed = 100f;
 	private float _yRotation = .30f;
 
-	//rotate y between yRot and -yRot
+	public GameObject tearPF;
+
+	void Start()
+	{
+		StartCoroutine (CreateTears ());
+	}
+
+	IEnumerator CreateTears(){
+
+		GameObject tear;
+
+		int counter=0;
+		float xOffset = 20f;
+		float yOffset = 10f;
+		float zOffset = 40f;
+
+		while (true) 
+		{
+			counter++;
+			if (counter % 2 == 1)
+				xOffset = -xOffset;
+			
+			tear = Instantiate (tearPF, new Vector3(transform.position.x-xOffset,transform.position.y-yOffset,transform.position.z-zOffset), Quaternion.identity, transform) as GameObject;
+			yield return new WaitForSeconds (.5f);
+			StartCoroutine(WaitAndDestroy(tear));
+		}
+	}
+
+	IEnumerator WaitAndDestroy(GameObject currentTear)
+	{
+		yield return new WaitForSeconds (2f);
+
+		Destroy (currentTear);
+	}
+		
 	void Update () {
 
 		if (Mathf.Abs(transform.localRotation.y) > _yRotation)
 			_speed = -_speed;
 
-		transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+//		transform.Rotate(Vector3.up, _speed * Time.deltaTime);
 	}
 }
