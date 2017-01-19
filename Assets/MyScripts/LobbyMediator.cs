15a9a85e0908c98cb405f25a74dfe3560b475e21
Marcus Choi <marcuslchoi@gmail.com>
2017-01-18 21:27:03 -0800

    generating score bars for display in lobby

--- Assets/MyScripts/LobbyMediator.cs
+++ Assets/MyScripts/LobbyMediator.cs
@@ -17,6 +17,10 @@ public class LobbyMediator : MonoBehaviour {
 	public Text Username;
 	public Button EarTrainingGame;
 	public GameObject LobbyPanel;
+	public Transform ScoresTransform;
+	public GameObject ScoreBarPF;
+
+	private List<Highscore> _myScores = new List<Highscore>();
 
 	// Use this for initialization
 	void Start () {
@@ -33,6 +37,7 @@ public class LobbyMediator : MonoBehaviour {
 
 		if (!String.IsNullOrEmpty (UserData.FirstName))
 			Username.text = UserData.FirstName + " "+ UserData.LastName.Substring (0, 1) + ".";
+
 	}
 
 	public void EarTrainingGameOnClick()
@@ -61,11 +66,14 @@ public class LobbyMediator : MonoBehaviour {
 			List<Highscore> items = response.Data;
 			Debug.Log("user id: " +UserData.UserId+ "MyScores items count: " + items.Count);
 
-			var myScores = items;
+			_myScores = items;
 
-			foreach (var highscore in myScores)
-				print ("MY SCORE! "+highscore.updatedAt+" "+highscore.score);
-			
+			foreach (var highscore in _myScores) {
+				print ("MY SCORE! " + highscore.updatedAt + " " + highscore.score);
+
+			}
+
+			//PopulateScores ();
 			//HasNewData = true;
 			//DisplayScores ();
 		}
@@ -75,7 +83,23 @@ public class LobbyMediator : MonoBehaviour {
 		}
 	}
 
+	public void PopulateScores()
+	{
+		var i = 0;
+		foreach (var highscore in _myScores) 
+		{
+			i++;
+			GameObject newObject = Instantiate (ScoreBarPF);
+			newObject.transform.SetParent (ScoresTransform, false);
+//			var yScale = (float)highscore.score / 10f;
+//			newObject.transform.localScale =  new Vector3(1f,yScale,1f);
+			newObject.transform.localPosition=new Vector3(i*27f,highscore.score);
 
+			var scoreBar = newObject.GetComponent<ScoreBar>();
+			scoreBar.SetData (highscore);
+
+		}
+	}
 
 	#endregion
 
---
+++ Assets/MyScripts/ScoreBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreBar : MonoBehaviour {
+
+	public Text ScoreText;
+	public Text DateText;
+
+	public void SetData (Highscore highscore)
+	{
+		ScoreText.text = highscore.score.ToString();
+		DateText.text = "date";
+	}
+}
